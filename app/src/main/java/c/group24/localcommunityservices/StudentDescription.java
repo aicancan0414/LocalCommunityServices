f19be1e1a0e0b832f27b0acca45a40bae87f973d
bkmur <bkmurray97@comcast.net>
2018-11-29 15:21:21 -0500

    Addition of Description page layouts

---
+++ app/src/main/java/c/group24/localcommunityservices/StudentDescription.java
@@ -0,0 +1,12 @@
+package c.group24.localcommunityservices;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StudentDescription extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
